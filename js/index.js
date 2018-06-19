e5c177fcec0864f923c7d42fa3a3a6bca5f03806
didykkatia <38005177+didykkatia@users.noreply.github.com>
2018-06-19 14:39:35 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,42 @@
+
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+$(function () {
+        var i = 0;
+        $('.mybtn').hover(
+            function () {
+                $(this).removeClass('bg-success').addClass('bg-danger');
+                i++;
+                $(this).attr("title", "Вы это уже сделали: " + i);
+            },
+            function () {
+                $(this).removeClass('bg-danger').addClass('bg-success');
+            }
+        );
+
+    });
+$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
